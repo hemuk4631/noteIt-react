2d7d700838650673b224c6924820a6d6e4953fd0
hemu kumar <hemuk4631@gmail.com>
2023-07-09 13:17:10 +0530

    server added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import { toast, ToastContainer } from "react-toastify";
 import Loader from "./Loader";
 
 function App() {
-  const LOCAL_URL="http://localhost:5000"
+  const LOCAL_URL="https://note-it-backend-sage.vercel.app"
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [addNotes, setAddNotes] = useState({
