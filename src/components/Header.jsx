4e0eff28f7cd5aa69c1459b648b1cf41d47ec6f8
hemuk4631 <hemuk4631@gmail.com>
2025-02-20 12:54:14 +0530

    temp change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Header() {
   return (
     <header className="text-white bg-success px-2 py-2">
-    
       <div className="container">
-        <h1 className="text-center"><i className="fa-solid fa-note-sticky fa-xs"></i>noteIt</h1>
+        <h1>Temp</h1>
+        <h1 className="text-center">
+          <i className="fa-solid fa-note-sticky fa-xs"></i>noteIt
+        </h1>
       </div>
     </header>
   );
